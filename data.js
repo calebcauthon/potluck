23cafe0ed416342e42bc768f061e628a0c719085
Caleb Cauthon <calebcauthon@gmail.com>
2018-07-06 21:16:55 -0500

    Add youtube videos

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 var grains = [
-  { name: "White Rice", image: "assets/white_rice.jpg" },
-  { name: "Brown Rice", image: "assets/brown_rice.jpg" }
+  { name: "White Rice", image: "assets/white_rice.jpg", recipeURL: "https://www.youtube.com/embed/Xx7sxWI9FNI" },
+  { name: "Brown Rice", image: "assets/brown_rice.jpg", recipeURL: "https://www.youtube.com/embed/BCOjMkarrxo" }
 ];
 
 var veggies = [
-  { name: "Broccoli", image: "assets/broccoli.jpg" },
-  { name: "Onions", image: "assets/onions.jpg" }
+  { name: "Broccoli", image: "assets/broccoli.jpg", recipeURL: "https://www.youtube.com/embed/wOQPqEPVjL4" },
+  { name: "Onions", image: "assets/onions.jpg", recipeURL: "https://www.youtube.com/embed/kBIM7Z5pWQU" }
 ];
 
 var beans = [
-  { name: "Pinto", image: "assets/pinto_beans.jpg" },
-  { name: "Black", image: "assets/black_beans.png" }
+  { name: "Pinto", image: "assets/pinto_beans.jpg", recipeURL: "https://www.youtube.com/embed/LPKJmFwFhD0" },
+  { name: "Black", image: "assets/black_beans.png", recipeURL: "https://www.youtube.com/embed/DFoYg5vxxsc" }
 ];
 
 var sauces = [
-  { name: "Chipotle Aoli", image: "assets/chipotle_aoli.jpg" },
-  { name: "Tahini", image: "assets/tahini_sauce.jpg" }
+  { name: "Chipotle Aoli", image: "assets/chipotle_aoli.jpg", recipeURL: "https://www.youtube.com/embed/VWIhPBxDDnU" },
+  { name: "Tahini", image: "assets/tahini_sauce.jpg", recipeURL: "https://www.youtube.com/embed/8Ia10OMvJWM" }
 ];
