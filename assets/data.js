c6580f157ad278391c9fe57e32eb65be3d936b41
Caleb Cauthon <calebcauthon@gmail.com>
2018-07-06 21:33:38 -0500

    Use sinatra

---
+++ assets/data.js
@@ -0,0 +1,19 @@
+var grains = [
+  { name: "White Rice", image: "white_rice.jpg", recipeURL: "https://www.youtube.com/embed/Xx7sxWI9FNI" },
+  { name: "Brown Rice", image: "brown_rice.jpg", recipeURL: "https://www.youtube.com/embed/BCOjMkarrxo" }
+];
+
+var veggies = [
+  { name: "Broccoli", image: "broccoli.jpg", recipeURL: "https://www.youtube.com/embed/wOQPqEPVjL4" },
+  { name: "Onions", image: "onions.jpg", recipeURL: "https://www.youtube.com/embed/kBIM7Z5pWQU" }
+];
+
+var beans = [
+  { name: "Pinto", image: "pinto_beans.jpg", recipeURL: "https://www.youtube.com/embed/LPKJmFwFhD0" },
+  { name: "Black", image: "black_beans.png", recipeURL: "https://www.youtube.com/embed/DFoYg5vxxsc" }
+];
+
+var sauces = [
+  { name: "Chipotle Aoli", image: "chipotle_aoli.jpg", recipeURL: "https://www.youtube.com/embed/VWIhPBxDDnU" },
+  { name: "Tahini", image: "tahini_sauce.jpg", recipeURL: "https://www.youtube.com/embed/8Ia10OMvJWM" }
+];
