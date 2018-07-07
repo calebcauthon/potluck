dc1816797ff5eaa3815defeddbd61bb4e16cda44
Caleb Cauthon <calebcauthon@gmail.com>
2018-07-06 20:20:17 -0500

    Add pictures

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 var grains = [
-  { name: "White Rice" },
-  { name: "Brown Rice" }
+  { name: "White Rice", image: "assets/white_rice.jpg" },
+  { name: "Brown Rice", image: "assets/brown_rice.jpg" }
 ];
 
 var veggies = [
-  { name: "Broccoli" },
-  { name: "Onions" }
+  { name: "Broccoli", image: "assets/broccoli.jpg" },
+  { name: "Onions", image: "assets/onions.jpg" }
 ];
 
 var beans = [
-  { name: "Pinto" },
-  { name: "Black" }
+  { name: "Pinto", image: "assets/pinto_beans.jpg" },
+  { name: "Black", image: "assets/black_beans.png" }
 ];
 
 var sauces = [
-  { name: "Chipotle Aoli" },
-  { name: "Tahini" }
+  { name: "Chipotle Aoli", image: "assets/chipotle_aoli.jpg" },
+  { name: "Tahini", image: "assets/tahini_sauce.jpg" }
 ];
