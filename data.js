747b18f72b864672b6f99294f42395ed9d4546bb
Caleb Cauthon <calebcauthon@gmail.com>
2018-07-06 19:45:49 -0500

    Add a variety of ingredients

---
+++ data.js
@@ -0,0 +1,19 @@
+var grains = [
+  { name: "White Rice" },
+  { name: "Brown Rice" }
+];
+
+var veggies = [
+  { name: "Broccoli" },
+  { name: "Onions" }
+];
+
+var beans = [
+  { name: "Pinto" },
+  { name: "Black" }
+];
+
+var sauces = [
+  { name: "Chipotle Aoli" },
+  { name: "Tahini" }
+];
