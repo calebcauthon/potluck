f26df2190c0b747eb53e11f583642e68cb6aa060
Caleb Cauthon <calebcauthon@gmail.com>
2018-07-08 11:08:05 -0500

    Put files in directories

---
+++ assets/app.js
@@ -0,0 +1,66 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    grain: null,
+    veggie1: null,
+    veggie2: null,
+    sauce: null,
+    bean: null,
+    isShowingRecipe: false,
+    isShowingIngredients: true,
+    ingredients: []
+  },
+  beforeMount() {
+    this.randomizeMeal();
+  },
+  methods: {
+    randomizeMeal: function() {
+      this.pickRandomGrain();
+      this.pickRandomVeggie();
+      this.pickRandomSauce();
+      this.pickRandomBean();
+      this.ingredients = [this.grain, this.veggie1, this.veggie2, this.sauce, this.bean];
+      this.showIngredients();
+    },
+    pickRandomGrain: function () {
+      const lastGrain = this.grain;
+
+      while(lastGrain == this.grain) {
+        this.grain = findRandomThing(grains);
+      }
+    },
+    pickRandomVeggie: function () {
+      const lastVeggie1 = this.veggie1;
+      const lastVeggie2 = this.veggie2;
+
+      while(lastVeggie1 == this.veggie1 || lastVeggie2 == this.veggie2) {
+        this.veggie1 = findRandomThing(veggies);
+        this.veggie2 = findRandomThing(veggies);
+
+        while (this.veggie2 == this.veggie1) {
+          this.veggie2 = findRandomThing(veggies);
+        }
+      }
+    },
+    pickRandomSauce: function () {
+      this.sauce = findRandomThing(sauces);
+    },
+    pickRandomBean: function () {
+      this.bean = findRandomThing(beans);
+    },
+    showRecipe: function() {
+      this.isShowingIngredients = false;
+      this.isShowingRecipe = true;
+    },
+    showIngredients: function() {
+      this.isShowingRecipe = false;
+      this.isShowingIngredients = true;
+    }
+  }
+});
+
+function findRandomThing(things) {
+  var max = things.length - 1;
+  var randomNumber = Math.floor(Math.random() * (Math.floor(max) + 1));
+  return things[randomNumber];
+}
---
+++ assets/firebase-init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC-_HY6R03VFD90vPjiVpGDGNCzYTxZu8k",
+  authDomain: "potluckfood-e.firebaseapp.com",
+  databaseURL: "https://potluckfood-e.firebaseio.com",
+  projectId: "potluckfood-e",
+  storageBucket: "",
+  messagingSenderId: "722653097613"
+};
+firebase.initializeApp(config);
